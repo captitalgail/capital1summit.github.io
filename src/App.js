33764f2fda7d7196bc6296999d9f6a3bfd52b927
gillarious <isabella.berryx3@gmail.com>
2018-10-28 21:07:01 -0400

    Added Title

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Container } from 'reactstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <Container className="App">
+        <h1>Capital One's Mind Sumo Challenge</h1>
+        <h2>By Isabella Berry</h2>
+      </Container>
     );
   }
 }
